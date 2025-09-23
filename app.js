36752a257b860ca31e0d0c48ce03635acfdff6bf
alvarob912 <alvarobijarra14@gmail.com>
2025-09-23 11:22:42 -0300

    conexion con MDB + html,css,js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express")
+const path = require('path')
+const mongoose = require('mongoose')
+const { timeStamp } = require("console")
 require('dotenv').config()
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).send("Servidor Express corriendo");
-  console.log("peticion recibida")
+mongoose.connect(
+    `mongodb+srv://alvarobijarra14_db_user:${process.env.PASSWORD}@cluster0.hxxrqiy.mongodb.net/stock-app?retryWrites=true&w=majority&appName=Cluster0`
+).then((result) => console.log("conexion exitosa a la base de datos"))
+.catch((err) => console.log(err))
+
+const productSchema = mongoose.Schema({
+  name: {type : String, required: true},
+  price: Number
+},
+{ timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
+
+app.post('/api/v1/products', async (req,res,next) => {
+  const newProduct = new Product({
+    name: req.body.name,
+    price: req.body.price
+  })
+  await newProduct.save()
+  res.status(201).json({ok: true})
 })
+
+
 
 const PORT = process.env.PORT || 4000
 
